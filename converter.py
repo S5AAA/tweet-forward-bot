06fd033be216c3b9a1d24cad7c06a3f296177a1a
CtrlVoid <tomarbiv1@gmail.com>
2020-02-28 15:30:04 +0200

    Working embeds for videos and images

--- converter.py
+++ converter.py
@@ -1,6 +1,24 @@
 #! python3
 
+import re
 import tweepy
 
+HTTP_PATTERN = r"(https?://[^ ]*)"
+
+EMPTY_EMBED = r"[​](\1)"
+LINK_EMBED = r"[\1](\1)"
+
+
 def twitter_to_telegram(status):
-    return status.text
+    text = status.text
+
+    print(f"TWITTER: {text} -> ", end="")
+    
+    if re.sub(HTTP_PATTERN, "", text, re.IGNORECASE):
+        text = re.sub(HTTP_PATTERN, EMPTY_EMBED, text, re.IGNORECASE)
+    else:
+        text = re.sub(HTTP_PATTERN, LINK_EMBED, text, re.IGNORECASE)
+
+    print(f"TELEGRAM: {text}")
+
+    return text
