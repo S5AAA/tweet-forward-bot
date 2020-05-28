48ec5220e6a2e6e47e4b214ceeef5e75f76e8645
S5AAA <tomarbiv1@gmail.com>
2020-05-28 22:42:33 +0300

    Logger, argparse

---
+++ logger.py
@@ -0,0 +1,20 @@
+import sys
+
+class Logger():
+    def __init__(self, logfile="bot_log.txt"):
+        self.terminal = sys.stdout
+        self.log = open(logfile, 'a')
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        self.terminal.flush()
+        self.log.flush()
+
+    def init(self):
+        sys.stdout = self
+
+    def finit(self):
+        self.log.close()
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 #! python3
 
-import twitter
+from logger import Logger
+import telegram_echo
 import converter
 import functools
-import telegram_echo
-import time
-
-CHANNEL = "-1001300279546"
-
-#ID = "250178637"   #MANNIE
-ID = "33473850"     #YNET
+import argparse
+import twitter
 
 RESTART_TIME = 30
 
@@ -19,10 +15,45 @@ def get_bot_echo(bot, convert_func):
 
     return bot_echo
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    optional = parser._action_groups.pop()
+    repeatable = parser.add_argument_group('required arguments')
+
+    repeatable.add_argument('-i', metavar='twitter_name', help='Name of Twitter accounts to echo', nargs='+', required=True)
+    repeatable.add_argument('-o', metavar='telegram_channel', help='Telegram channel IDs to echo into', nargs='+', required=True)
+
+    optional.add_argument('-r', metavar='reset_time', help='Time the bot sleeps between rate limit errors or manual restarts', default=30, type=int)
+
+    parser._action_groups.append(optional)
+
+    return parser.parse_args()
+
 def main():
-    telegram_bot = telegram_echo.TelegramEchoBot([CHANNEL])
+    args = parse_args()
     
-    twitter_bot = twitter.CustomStreamListener([ID], [get_bot_echo(telegram_bot, converter.twitter_to_telegram)])
+    twitter_names = args.i
+    telegram_channels = args.o
+    reset_time = args.r
+
+    print(f"""Starting with:
+twitter_names = {twitter_names},
+telegram_channels = {telegram_channels},
+reset_time = {reset_time}""")
+
+    print(f"Getting ids for {twitter_names}")
+    twitter_ids = twitter.ids_from_names(*twitter_names)
+    print(f"Got {twitter_ids}")
+
+    telegram_bot = telegram_echo.TelegramEchoBot(telegram_channels)
+    
+    twitter_bot = twitter.CustomStreamListener(twitter_ids, [get_bot_echo(telegram_bot, converter.twitter_to_telegram)])
+    
+    print(f"Initializing logger...")
+    logger = Logger()
+    logger.init()
 
     while True:
         try:
@@ -33,11 +64,16 @@ def main():
             print(e)
 
         try:
-            print(f"Restarting bot in {RESTART_TIME}s... (Ctrl+C to exit)")
-            time.sleep(RESTART_TIME)
+            print(f"Restarting bot in {reset_time}s... (Ctrl+C to exit)")
+            time.sleep(reset_time)
         except KeyboardInterrupt:
-            print("\nKeyboard interrupt while waiting for restart. Exiting bot.")
-            exit()
+            print("\nKeyboard interrupt while waiting for restart. Shutting down.")
+            break
+
+    print("Shutting down logger...")
+    logger.finit()
+
+    print("Exiting")
 
 if __name__ == "__main__":
     main()
